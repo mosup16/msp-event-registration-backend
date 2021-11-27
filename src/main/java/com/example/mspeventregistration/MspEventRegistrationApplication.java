e38a751053dbb68b34efa31286fc7d63f0c60f77
mosup <mosup16@gmail.com>
2021-11-27 18:22:17 +0200

    initial commit

---
+++ src/main/java/com/example/mspeventregistration/MspEventRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.mspeventregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MspEventRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MspEventRegistrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mspeventregistration/MspEventRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mspeventregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MspEventRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
