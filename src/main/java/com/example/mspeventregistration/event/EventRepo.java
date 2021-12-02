83d4175f113302966a8c796e532ae92dfb15641f
mosup <mosup16@gmail.com>
2021-12-02 08:00:19 +0200

    add search by name support for events

--- src/main/java/com/example/mspeventregistration/event/EventRepo.java
+++ src/main/java/com/example/mspeventregistration/event/EventRepo.java
@@ -1,5 +1,6 @@
 package com.example.mspeventregistration.event;
 
+import io.vavr.control.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,5 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin
 @RepositoryRestResource(excerptProjection = EventProjection.class)
 public interface EventRepo extends JpaRepository<Event,Long> {
+
+    Option<Event> findByEventName(String name);
 
 }
